8bd8c1557b6528b6ed4514618995997709bac3b8
Lukasz Wisniewski <luke.cherrish@gmail.com>
2012-12-17 14:03:17 +0100

    Poll.java jdocs

--- library/src/com/eyeem/poll/Poll.java
+++ library/src/com/eyeem/poll/Poll.java
@@ -9,41 +9,139 @@ import android.util.Log;
 
 import com.eyeem.storage.Storage;
 
+/**
+ * Base class for polling. Works in combination with {@link Storage}
+ * class.
+ *
+ * @param <T>
+ */
 public abstract class Poll<T> {
 
    public static boolean DEBUG = false;
 
+   /**
+    * No poll has been made yet so we don't know
+    * anything.
+    */
    public static final int STATE_UNKNOWN = -1;
+
+   /**
+    * Poll is looking good.
+    */
    public static final int STATE_OK = 0;
+
+   /**
+    * Poll has no content.
+    */
    public static final int STATE_NO_CONTENT = 1;
+
+   /**
+    * Poll has no content due to lack of connectivity.
+    */
    public static final int STATE_NO_CONNECTION = 2;
 
+   /**
+    * Associated storage.
+    */
    protected Storage<T>.List list;
 
+   /**
+    * No more items to poll
+    */
    protected boolean exhausted;
+
+   /**
+    * Indicates whether we're polling or not.
+    */
    protected boolean polling;
+
+   /**
+    * Last time successful {@link #update(Listener)} occured.
+    * UNIX time ms.
+    */
    protected long lastTimeUpdated;
+
+   /**
+    * How often should polling happen.
+    */
    protected long refreshPeriod;
+
+   /**
+    * Represents state in which poll is.
+    */
    private int state = STATE_UNKNOWN;
 
+   /**
+    * Fetches new items
+    * @return
+    * @throws Throwable
+    */
    protected abstract ArrayList<T> newItems() throws Throwable;
+
+   /**
+    * Fetches older items
+    * @return
+    * @throws Throwable
+    */
    protected abstract ArrayList<T> oldItems() throws Throwable;
+
+   /**
+    * Appends new items to the {@link #list}
+    * @param newItems
+    * @param listener
+    * @return
+    */
    protected abstract int appendNewItems(ArrayList<T> newItems, Poll.Listener listener);
+
+   /**
+    * Appends old items to the {@link #list}
+    * @param oldItems
+    * @param listener
+    * @return
+    */
    protected abstract int appendOldItems(ArrayList<T> oldItems, Poll.Listener listener);
+
+   /**
+    * @param ctx
+    * @param newCount
+    * @return Message associated with successful update.
+    */
    protected abstract String getSuccessMessage(Context ctx, int newCount);
 
+   /**
+    * Fetch new items task
+    */
    private RefreshTask updating;
+
+   /**
+    * Fetch older items task
+    */
    private RefreshTask fetchingMore;
 
+   /**
+    * Indicates whether {@link Poll} should update or not. Default policy
+    * is to update if {@link #lastTimeUpdated} happened more than
+    * {@link #refreshPeriod} time ago.
+    * @return
+    */
    public boolean shouldUpdate() {
       return (System.currentTimeMillis() - refreshPeriod) > lastTimeUpdated;
    }
 
+   /**
+    * Updates poll if it {@link #shouldUpdate()}
+    * @param listener
+    */
    public void updateIfNecessary(Listener listener) {
       if (shouldUpdate())
          update(listener);
    }
 
+   /**
+    * Updates poll always. Please consider using
+    * {@link #updateIfNecessary(Listener)}
+    * @param listener
+    */
    public synchronized void update(final Listener listener) {
       if (updating == null || updating.working == false) {
          updating = new RefreshTask() {
@@ -118,14 +216,54 @@ public abstract class Poll<T> {
    public void setStorage(Storage<T>.List list) { this.list = list; }
    public Storage<T>.List getStorage() { return list; }
 
+   /**
+    * Poll refresh interface
+    */
    public interface Listener {
+      /**
+       * Poll started
+       */
       public void onStart();
+
+      /**
+       * Poll encountered an error
+       * @param error
+       */
       public void onError(Throwable error);
+
+      /**
+       * Poll was successful
+       * @param newCount number of new items
+       */
       public void onSuccess(int newCount);
+
+      /**
+       * Poll is already polling
+       */
       public void onAlreadyPolling();
+
+      /**
+       * Poll's state has changed
+       * @param state
+       */
       public void onStateChanged(int state);
+
+      /**
+       * Poll has exhausted.
+       */
       public void onExhausted();
+
+      /**
+       * Facility to tell Poll about currently viewed
+       * id
+       * @return
+       */
       public String getCurrentId();
+
+      /**
+       * Trim action is about to happen
+       * @param currentId
+       */
       public void onTrim(String currentId);
    }
 
@@ -175,7 +313,6 @@ public abstract class Poll<T> {
          working = false;
       }
 
-
       protected void onStateChanged() {
          for (Listener listener : listeners)
             listener.onStateChanged(state);
@@ -201,14 +338,23 @@ public abstract class Poll<T> {
       }
    }
 
+   /**
+    * @return Poll's state
+    */
    public int getState() {
       if (list != null && !list.isEmpty())
          return state = STATE_OK;
       return state;
    }
 
+   /**
+    * @return tells whether it's ok to persist list in it's current state
+    */
    public boolean okToSave() { return true; }
 
+   /**
+    * Sets {@link #lastTimeUpdated} value to 0.
+    */
    public void resetLastTimeUpdated() {
       lastTimeUpdated = 0;
    }
